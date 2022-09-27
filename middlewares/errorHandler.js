726b46188472e32de613f4b351595dae8f002fef
Aleksey Barabanov <forarbaiten@yandex.ru>
2022-09-28 01:03:16 +0300

    feat/errors. fixing bugs

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,8 @@
 /* eslint-disable linebreak-style */
 const errorHandler = (err, req, res, next) => {
   res.status(err.code).send({ message: err.message });
-  console.log(err.code, err.message)
+  // eslint-disable-next-line no-console
+  console.log(err.code, err.message);
   next();
 };
 module.exports = errorHandler;
